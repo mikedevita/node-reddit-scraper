cbdf1c77d53ea003fb7f075e798215290e9dc634
Mike DeVita <mike@relative.media>
2015-11-30 09:26:39 -0700

    refactor of code
    Signed-off-by: Mike DeVita <mike@relative.media>

--- index.js
+++ index.js
@@ -1,37 +1,30 @@
-'use strict';
-
-
-var winston = require('winston');
-var _       = require('lodash');
 var async   = require('async');
-var path    = require('path');
 var config  = require('./config');
 var Reddit  = require('./lib/reddit');
-var Imgur   = require('./lib/imgur');
 var Scraper = require('./lib/scraper');
+var Log  = require('./lib/logger');
 
-var log = new winston.Logger({
-  transports: [new winston.transports.Console(config.log)]
-});
 
 var subreddits = config.subreddits;
 
-async.eachSeries(subreddits, function(subreddit, subCallback){
-  log.info("Processing subreddit " + subreddit.name);
-  Reddit.getData(config.reddit.url, subreddit, function(err, pages){
-	  
-	  if (err) throw err;
-	  
-	  if( Array.isArray(pages) ){
-	    async.eachLimit(pages, 1, function(page, callback){
-	      Scraper.scrape(subreddit, page, callback);
-	    }, function(err){
-	      if(err){
-		    log.error("Error processing " + subreddit.name + ": " + err);
-	      }
-	      subCallback(err);
-	    });
-	  }
+Log.info('Initializing ' + config.reddit.userAgent);
+async.each(subreddits, function (subreddit, subCallback) {
+  Log.info('Processing subreddit ' + subreddit.name);
+  Reddit.getData(config.reddit.url, subreddit, function (err, pages) {
+
+    if (err) {
+      throw err;
+    }
+
+    if ( Array.isArray(pages) ) {
+      async.eachLimit(pages, 1, function (page, callback) {
+        Scraper.scrape(subreddit, page, callback);
+      }, function (err) {
+        if (err) {
+          Log.error('Error processing ' + subreddit.name + ': ' + err);
+        }
+        subCallback(err);
+      });
+    }
   });
 });
-
