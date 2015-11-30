43a7e25dac77ec07917270a001846839b3239b3a
Mike DeVita <mike@relative.media>
2015-11-30 13:12:13 -0700

    forgot to enable all tests, whoops
    Signed-off-by: Mike DeVita <mike@relative.media>

--- test/unit/Reddit.test.js
+++ test/unit/Reddit.test.js
@@ -4,7 +4,7 @@ var should = require('should');
 
 describe('Reddit Lib', function () {
 
-  describe.only('buildUrl()', function () {
+  describe('buildUrl()', function () {
     it('should return a valid reddit api url', function (done) {
       var subreddit = {
         storeByUser: true,
