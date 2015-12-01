da7b24460390563a6f5d734dbf3a2e8f0db4a5c5
Mike DeVita <mike@relative.media>
2015-12-01 11:03:55 -0700

    more improvements to the codebase
    additional logging that shows stats.
    
    Signed-off-by: Mike DeVita <mike@relative.media>

--- index.js
+++ index.js
@@ -7,24 +7,41 @@ var pages   = require('./pages.json');
 var _       = require('lodash');
 
 var domains = [];
+var services = [];
 var subreddits = config.subreddits;
 
+var stats = {
+  totalImagesDownloaded: 0,
+  totalImagesExisting: 0,
+  totalPagesScraped: 0,
+  totalSubredditsScraped: 0
+};
+
+console.log();
+console.log('-----------------------------------------------');
 Log.info('Initializing ' + config.reddit.userAgent);
 
 
 if ( config.gfycat.enabled ) {
+  services.push('gfycat');
   domains.push(config.gfycat.domains);
 }
 
 if ( config.imgur.enabled ) {
+  services.push('imgur');
   domains.push(config.imgur.domains);
 }
 
 domains = _.flatten(domains);
-Log.debug('Index - allowed domains are: ' + domains);
+var sr = subreddits.map(function (subreddit) {
+  return subreddit.name;
+});
 
+Log.info('  Subreddits: ' + sr);
+Log.info('  Services: ' + services);
+console.log('-----------------------------------------------');
 async.each(subreddits, function (subreddit, subCallback) {
-  Log.info('Processing subreddit ' + subreddit.name);
+  Log.info('Started Processing /r/' + subreddit.name);
 
   function scraperCallback(err, pages) {
 
@@ -32,15 +49,49 @@ async.each(subreddits, function (subreddit, subCallback) {
       throw err;
     }
 
+    // iterate over each page of each subreddit
     if ( Array.isArray(pages) ) {
-      async.each(pages, function (page, callback) {
-        Log.debug('Index - Scraping page');
-        Scraper.scrape(subreddit, page, { domains: domains }, callback);
+      var Images = [];
+      var imagesDownloaded = 0;
+      var existingImagesCount = 0;
+      stats.totalSubredditsScraped++;
+      async.forEachOf(pages, function (page, index, callback) {
+        // scrape the subreddit page
+        Scraper.scrape(subreddit, page, { domains: domains }, function (err, images) {
+
+          images = _.flatten(images);
+          Images.push( images );
+          stats.totalPagesScraped++;
+          callback();
+        });
       }, function (err) {
         if (err) {
           Log.error('Error processing ' + subreddit.name + ': ' + err);
         }
-        subCallback(err);
+        Images = _.flatten(Images);
+        async.each(Images, function (image, cb) {
+          Scraper.download(image.url, image.imgPath, function (err, wasDownloaded) {
+            if (wasDownloaded) {
+              imagesDownloaded++;
+            } else {
+              existingImagesCount++;
+            }
+            setTimeout(function () {
+              cb(null);
+            }, 3000);
+          });
+        }, function (err) {
+          if (err) {
+            Log.warn(err);
+          }
+
+          Log.info('Finished with /r/' + subreddit.name);
+          Log.info('  Downloaded ' + imagesDownloaded, 'images.');
+          Log.info('  ' + existingImagesCount, 'images already existing.');
+          stats.totalImagesDownloaded += imagesDownloaded;
+          stats.totalImagesExisting += existingImagesCount;
+          subCallback(err);
+        });
       });
     }
   }
@@ -51,4 +102,13 @@ async.each(subreddits, function (subreddit, subCallback) {
   } else {
     Reddit.getData(config.reddit.url, subreddit, scraperCallback);
   }
+}, function () {
+  console.log();
+  console.log('--------------------------');
+  Log.info('Scraper run finished');
+  Log.info('  Total Images Downloaded:', stats.totalImagesDownloaded);
+  Log.info('  Total Pre-existing Images:', stats.totalImagesExisting);
+  Log.info('  Total Subreddits Scraped:', stats.totalSubredditsScraped);
+  Log.info('  Total Pages Scraped:', stats.totalPagesScraped);
+  console.log('--------------------------');
 });
